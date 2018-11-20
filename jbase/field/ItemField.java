e3346f6489408606dd754d1ad6856b1e26d1d515
Bryan McClain <bamcclain10@gmail.com>
2018-11-20 02:02:15 -0500

    Small update to Item Field

--- jbase/field/ItemField.java
+++ jbase/field/ItemField.java
@@ -1,24 +1,29 @@
 package jbase.field;
 
+import java.io.Serializable;
+
 /**
  * Represents an "Item" in our database (field owned by a key)
  * @author Bryan McClain
  */
-public class ItemField<T extends Serializable> extends Field<T> {
+public class ItemField<T extends Serializable> extends Field<T> implements Serializable {
 
 	private final KeyField owner;
+	private ArrayList<T> values;
 
 
 	/**
-	 * Construct a new item field in the database
+	 * Construct a new item field in the database.
+	 *  Depth is inherited from the owner field.
 	 *
 	 * @param db The database for this field
 	 * @param name The name of this field
 	 * @param owner Key that owns this field
 	 */
 	public ItemField(Database db, String name, KeyField owner) {
-		super(db,name,FieldType.ITEM,owner.getDepth());
+		super(db,name,FieldType.ITEM);
 		this.owner = owner;
+		this.values = new ArrayList<T>(owner.getDepth());
 	}
 
 }
